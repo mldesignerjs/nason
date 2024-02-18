bb6ef564a50536382d374c547da615e144127255
mldesignerjs <mldesignerjs@gmail.com>
2024-02-18 22:27:03 +0700

    thirt-commit

--- app/detail/[sku]/page.tsx
+++ app/detail/[sku]/page.tsx
@@ -1,16 +1,4 @@
-import type { Metadata } from 'next'
 import DetailPage from './DetailPage'
-
-type Props = {
-    params: { sku: string }
-}
-
-export function generateMetadata({ params }: Props): Metadata {
-    const sku = params.sku
-    return {
-        title: sku,
-    }
-}
 
 export default function Detail() {
     return (
